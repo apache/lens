e21a4083939a37c4661ffc4a47f7069804925584
jaideep.dhok <jaideep.dhok@inmobi.com>
2013-09-27 10:44:31 +0530

    Fix grill build after changes to addPartition in CubeMetastoreClient

--- grill-yoda/src/main/java/com/inmobi/yoda/cube/ddl/PopulatePartitions.java
+++ grill-yoda/src/main/java/com/inmobi/yoda/cube/ddl/PopulatePartitions.java
@@ -174,7 +174,7 @@ public class PopulatePartitions {
               Map<String, Date> partitionTimestamps = new HashMap<String, Date>();
               partitionTimestamps.put(CubeDDL.PART_KEY_IT, dt);
               System.out.println("Adding partitions for Path" + partPath);
-              client.addPartition(fact, storage, updatePeriod, partitionTimestamps);
+              client.addPartition(fact, storage, updatePeriod, partitionTimestamps, null);
             }
           } else {
             if (entry.getKey().equalsIgnoreCase(CubeDDL.YODA_PIE_STORAGE)) {
@@ -195,7 +195,7 @@ public class PopulatePartitions {
                   partitionTimestamps.put(CubeDDL.PART_KEY_IT, it);
                   partSpec.put(CubeDDL.PART_KEY_COLO, colo); 
                   System.out.println("Adding partitions for Path" + cstat.getPath());
-                  client.addPartition(fact, storage, updatePeriod, partitionTimestamps, partSpec);
+                  client.addPartition(fact, storage, updatePeriod, partitionTimestamps, partSpec, null);
                 }
               } else {
                 partPath = new Path(new Path(new Path(summariesPath, factPathName),
@@ -242,7 +242,7 @@ public class PopulatePartitions {
                       partSpec.put(CubeDDL.PART_KEY_ET, estat.getPath().getName()); 
                     }
                     System.out.println("Adding partitions for Path" + estat.getPath());
-                    client.addPartition(fact, storage, updatePeriod, partitionTimestamps, partSpec);
+                    client.addPartition(fact, storage, updatePeriod, partitionTimestamps, partSpec, null);
                   }
                 }
               }
