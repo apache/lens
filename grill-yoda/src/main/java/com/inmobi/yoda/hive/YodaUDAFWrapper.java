4da95851a3885128bb2f749dfe2318fdd37d1d06
jaideep.dhok <jaideep.dhok@inmobi.com>
2013-09-30 15:44:20 +0530

    Add Yoda FPJAMR dependency

--- grill-yoda/src/main/java/com/inmobi/yoda/hive/YodaUDAFWrapper.java
+++ grill-yoda/src/main/java/com/inmobi/yoda/hive/YodaUDAFWrapper.java
@@ -2,9 +2,23 @@ package com.inmobi.yoda.hive;
 
 import com.inmobi.dw.yoda.mr.aggregators.Aggregator;
 import org.apache.hadoop.hive.ql.exec.UDAF;
+import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
 
 public class YodaUDAFWrapper extends UDAF {
-  Aggregator yodaAggregator;
+  public static class UDAFState {
+    Aggregator yodaAggregator;
+  }
 
-  public static class IntegerUDAF
+  public static class YodaUDAFEvaluator implements UDAFEvaluator {
+    UDAFState state;
+
+    public YodaUDAFEvaluator() {
+      super();
+      state = new UDAFState();
+    }
+
+    @Override
+    public void init() {
+    }
+  }
 }
