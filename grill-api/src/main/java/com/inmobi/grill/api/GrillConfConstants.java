786c344ebae6533cc5727736b96dcfa31a7c403a
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2013-10-21 11:57:39 +0530

    Add misisng class

---
+++ grill-api/src/main/java/com/inmobi/grill/api/GrillConfConstants.java
@@ -0,0 +1,11 @@
+package com.inmobi.grill.api;
+
+public class GrillConfConstants {
+
+  public static final String PREPARE_ON_EXPLAIN = "grill.doprepare.on.explain";
+
+  public static final Boolean DEFAULT_PREPARE_ON_EXPLAIN = true;
+
+  public static final String ENGINE_DRIVER_CLASSES = "grill.drivers";
+
+}
