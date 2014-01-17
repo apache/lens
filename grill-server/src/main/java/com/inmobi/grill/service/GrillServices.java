f73dc0d742f75044fbfef712afb4de199b89f783
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2014-01-17 16:38:04 +0530

    Remove testng suite file to allow all the tests to run

--- grill-server/src/main/java/com/inmobi/grill/service/GrillServices.java
+++ grill-server/src/main/java/com/inmobi/grill/service/GrillServices.java
@@ -43,46 +43,55 @@ public class GrillServices extends CompositeService {
 
   @Override
   public synchronized void init(HiveConf hiveConf) {
-    if (getServiceState() == STATE.INITED) {
-      LOG.info("GrillServices already initialized");
-      return;
-    }
+    if (getServiceState() == STATE.NOTINITED) {
+      conf = hiveConf;
+      conf.setVar(HiveConf.ConfVars.HIVE_SESSION_IMPL_CLASSNAME, GrillSessionImpl.class.getCanonicalName());
+      cliService = new CLIService();
 
-    conf = hiveConf;
-    conf.setVar(HiveConf.ConfVars.HIVE_SESSION_IMPL_CLASSNAME, GrillSessionImpl.class.getCanonicalName());
-    cliService = new CLIService();
+      // Add default services
+      addService(cliService);
+      addService(new EventServiceImpl(GrillEventService.NAME));
 
-    // Add default services
-    addService(cliService);
-    addService(new EventServiceImpl(GrillEventService.NAME));
-
-    // Add configured services, these are instances of GrillService which need a CLIService instance
-    // for session management
-    String[] serviceNames = conf.getStrings(GrillConfConstants.GRILL_SERVICE_NAMES);
-    for (String sName : serviceNames) {
-      try {
-        Class<? extends GrillService> serviceClass = conf.getClass(
-          GrillConfConstants.getServiceImplConfKey(sName), null,
-          GrillService.class);
-        LOG.info("Adding " + sName + " service with " + serviceClass);
-        Constructor<?> constructor = serviceClass.getConstructor(CLIService.class);
-        GrillService service  = (GrillService) constructor.newInstance(new Object[]
-          {cliService});
-        addService(service);
-        grillServices.add(service);
-      } catch (Exception e) {
-        LOG.warn("Could not add service:" + sName, e);
-        throw new WebApplicationException(e);
+      // Add configured services, these are instances of GrillService which need a CLIService instance
+      // for session management
+      String[] serviceNames = conf.getStrings(GrillConfConstants.GRILL_SERVICE_NAMES);
+      for (String sName : serviceNames) {
+        try {
+          Class<? extends GrillService> serviceClass = conf.getClass(
+              GrillConfConstants.getServiceImplConfKey(sName), null,
+              GrillService.class);
+          LOG.info("Adding " + sName + " service with " + serviceClass);
+          Constructor<?> constructor = serviceClass.getConstructor(CLIService.class);
+          GrillService service  = (GrillService) constructor.newInstance(new Object[]
+              {cliService});
+          addService(service);
+          grillServices.add(service);
+        } catch (Exception e) {
+          LOG.warn("Could not add service:" + sName, e);
+          throw new WebApplicationException(e);
+        }
       }
-    }
 
-    for (Service svc : getServices()) {
-      services.put(svc.getName(), svc);
-    }
+      for (Service svc : getServices()) {
+        services.put(svc.getName(), svc);
+      }
 
-    // This will start all services in the order they were added
-    super.init(conf);
-    LOG.info("Initialized grill services: " + services.keySet().toString());
+      // This will start all services in the order they were added
+      super.init(conf);
+      LOG.info("Initialized grill services: " + services.keySet().toString());
+    }
+  }
+
+  public synchronized void start() {
+    if (getServiceState() != STATE.STARTED) {
+      super.start();
+    }
+  }
+
+  public synchronized void stop() {
+    if (getServiceState() != STATE.STOPPED) {
+      super.stop();
+    }
   }
 
   public static GrillServices get() {
