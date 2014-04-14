3c9f3b983a183d509b0c952e627e75c45ee0827f
Amareshwari Sriramdasu <amareshwari.sr@inmobi.com>
2014-04-14 16:05:44 +0530

    Update consistent state filter

--- grill-server/src/main/java/com/inmobi/grill/server/ConsistentStateFilter.java
+++ grill-server/src/main/java/com/inmobi/grill/server/ConsistentStateFilter.java
@@ -2,43 +2,18 @@ package com.inmobi.grill.server;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.hive.service.Service;
-
-public class ConsistentStateFilter  implements Filter {
-
-  public static final Log LOG = LogFactory.getLog(AuthenticationFilter.class);
+public class ConsistentStateFilter implements ContainerRequestFilter {
 
   @Override
-  public void destroy() {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
-    if (GrillServices.get().getServiceState().equals(Service.STATE.STARTED) &&
-        !GrillServices.get().isStopping() ) {
-      chain.doFilter(request, response);
-    } else {
-      throw new IllegalStateException("Server is going down");
+  public void filter(ContainerRequestContext requestContext) throws IOException {
+    if (GrillServices.get().isStopping()) {
+      requestContext.abortWith(Response.status(Status.SERVICE_UNAVAILABLE)
+          .entity("Server is going down").build());
     }
   }
-
-  @Override
-  public void init(FilterConfig config) throws ServletException {
-    // TODO Auto-generated method stub
-    
-  }
-
-
 }
