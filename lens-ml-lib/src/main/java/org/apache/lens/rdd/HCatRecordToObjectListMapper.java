2e07435374a7a2d5fd700124568bd7bb58bee4a6
jdhok <jaideep.dhok@inmobi.com>
2014-10-16 14:01:28 +0530

    Moved RDD client into lens-ml-lib

---
+++ lens-ml-lib/src/main/java/org/apache/lens/rdd/HCatRecordToObjectListMapper.java
@@ -0,0 +1,26 @@
+package org.apache.lens.rdd;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hive.hcatalog.data.HCatRecord;
+import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HCatRecordToObjectListMapper implements Function<Tuple2<WritableComparable, HCatRecord>, List<Object>> {
+  @Override
+  public List<Object> call(Tuple2<WritableComparable, HCatRecord> hcatTuple) throws Exception {
+    HCatRecord record = hcatTuple._2();
+
+    if (record == null) {
+      return null;
+    }
+
+    List<Object> row = new ArrayList<Object>(record.size());
+    for (int i = 0; i < record.size(); i++) {
+      row.add(record.get(i));
+    }
+    return row;
+  }
+}
