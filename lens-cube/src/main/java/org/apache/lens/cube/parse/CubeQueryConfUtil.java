532494c0c20016186ab55b85655ad2c66c452bb6
Amareshwari Sriramdasu <amareshwari.sr@inmobi.com>
2014-10-28 15:21:11 +0530

    Config rename from cube.query to lens.cube.query

--- lens-cube/src/main/java/org/apache/lens/cube/parse/CubeQueryConfUtil.java
+++ lens-cube/src/main/java/org/apache/lens/cube/parse/CubeQueryConfUtil.java
@@ -33,23 +33,23 @@ public class CubeQueryConfUtil {
   public static final String UPDATE_PERIODS_SFX = ".updateperiods";
   public static final String FACT_TABLES_SFX = ".facttables";
   public static final String STORAGE_KEY_PFX = ".storage.";
-  public static final String VALID_PFX = "cube.query.valid.";
-  public static final String VALID_FACT_PFX = "cube.query.valid." + "fact.";
-  public static final String TIMEDIM_TO_PART_MAPPING_PFX = "cube.query.timedim.partition.";
+  public static final String VALID_PFX = "lens.cube.query.valid.";
+  public static final String VALID_FACT_PFX = VALID_PFX + "fact.";
+  public static final String TIMEDIM_TO_PART_MAPPING_PFX = "lens.cube.query.timedim.partition.";
 
-  public static final String VALID_STORAGE_DIM_TABLES = "cube.query.valid." + "dim.storgaetables";
-  public static final String DRIVER_SUPPORTED_STORAGES = "cube.query.driver." + "supported.storages";
-  public static final String FAIL_QUERY_ON_PARTIAL_DATA = "cube.query.fail.if.data.partial";
-  public static final String NON_EXISTING_PARTITIONS = "cube.query.nonexisting.partitions";
-  public static final String ADD_NON_EXISTING_PARTITIONS = "cube.query.add.nonexisting.partitions";
-  public static final String ENABLE_MULTI_TABLE_SELECT = "cube.query.enable.multi.table.select";
-  public static final String QUERY_MAX_INTERVAL = "cube.query.max.interval";
-  public static final String PROCESS_TIME_PART_COL = "cube.query.process.time" + ".partition.column";
-  public static final String LOOK_AHEAD_PT_PARTS_PFX = "cube.query.lookahead.ptparts.forinterval.";
-  public static final String ENABLE_GROUP_BY_TO_SELECT = "cube.query.promote.groupby.toselect";
-  public static final String ENABLE_SELECT_TO_GROUPBY = "cube.query.promote.select.togroupby";
+  public static final String VALID_STORAGE_DIM_TABLES = "lens.cube.query.valid." + "dim.storgaetables";
+  public static final String DRIVER_SUPPORTED_STORAGES = "lens.cube.query.driver." + "supported.storages";
+  public static final String FAIL_QUERY_ON_PARTIAL_DATA = "lens.cube.query.fail.if.data.partial";
+  public static final String NON_EXISTING_PARTITIONS = "lens.cube.query.nonexisting.partitions";
+  public static final String ADD_NON_EXISTING_PARTITIONS = "lens.cube.query.add.nonexisting.partitions";
+  public static final String ENABLE_MULTI_TABLE_SELECT = "lens.cube.query.enable.multi.table.select";
+  public static final String QUERY_MAX_INTERVAL = "lens.cube.query.max.interval";
+  public static final String PROCESS_TIME_PART_COL = "lens.cube.query.process.time" + ".partition.column";
+  public static final String LOOK_AHEAD_PT_PARTS_PFX = "lens.cube.query.lookahead.ptparts.forinterval.";
+  public static final String ENABLE_GROUP_BY_TO_SELECT = "lens.cube.query.promote.groupby.toselect";
+  public static final String ENABLE_SELECT_TO_GROUPBY = "lens.cube.query.promote.select.togroupby";
 
-  public static final String REPLACE_TIMEDIM_WITH_PART_COL = "cube.query.replace.timedim";
+  public static final String REPLACE_TIMEDIM_WITH_PART_COL = "lens.cube.query.replace.timedim";
   public static final boolean DEFAULT_MULTI_TABLE_SELECT = true;
   public static final int DEFAULT_LOOK_AHEAD_PT_PARTS = 1;
   public static final boolean DEFAULT_ENABLE_GROUP_BY_TO_SELECT = false;
@@ -91,15 +91,15 @@ public class CubeQueryConfUtil {
     return list;
   }
 
-  public static final String DISABLE_AUTO_JOINS = "hive.cube.disable.auto.join";
+  public static final String DISABLE_AUTO_JOINS = "lens.cube.query.disable.auto.join";
   public static final boolean DEFAULT_DISABLE_AUTO_JOINS = true;
-  public static final String JOIN_TYPE_KEY = "hive.cube.join.type";
-  public static final String DISABLE_AGGREGATE_RESOLVER = "hive.cube.disable.aggregate.resolver";
+  public static final String JOIN_TYPE_KEY = "lens.cube.query.join.type";
+  public static final String DISABLE_AGGREGATE_RESOLVER = "lens.cube.query.disable.aggregate.resolver";
   public static final boolean DEFAULT_DISABLE_AGGREGATE_RESOLVER = true;
-  public static final String LIGHTEST_FACT_FIRST = "cube.query.pick.lightest.fact.first";
+  public static final String LIGHTEST_FACT_FIRST = "lens.cube.query.pick.lightest.fact.first";
   public static final boolean DEFAULT_LIGHTEST_FACT_FIRST = false;
-  public static final String TIME_RANGE_WRITER_CLASS = "cube.query.time.range.writer.class";
+  public static final String TIME_RANGE_WRITER_CLASS = "lens.cube.query.time.range.writer.class";
   public static final Class<? extends TimeRangeWriter> DEFAULT_TIME_RANGE_WRITER = ORTimeRangeWriter.class
       .asSubclass(TimeRangeWriter.class);
-  public static final String PART_WHERE_CLAUSE_DATE_FORMAT = "cube.query.partition.where.clause.format";
+  public static final String PART_WHERE_CLAUSE_DATE_FORMAT = "lens.cube.query.partition.where.clause.format";
 }
