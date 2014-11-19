6f0b0d9c56702220777963fceb28b26845d6e825
Amareshwari Sriramdasu <amareshwari.sr@inmobi.com>
2014-11-19 18:07:15 +0530

    LENS-38 : Fixes getting latestdate of cube api to look in current session (Prashant Gupta via amareshwari)

--- lens-server/src/main/java/org/apache/lens/server/metastore/CubeMetastoreServiceImpl.java
+++ lens-server/src/main/java/org/apache/lens/server/metastore/CubeMetastoreServiceImpl.java
@@ -1319,6 +1319,7 @@ public class CubeMetastoreServiceImpl extends LensService implements CubeMetasto
   @Override
   public Date getLatestDateOfCube(LensSessionHandle sessionid, String cubeName, String timeDimension)
       throws LensException, HiveException {
+    acquire(sessionid);
     // get the partitionColumn corresponding to timeDimension passed
     CubeInterface ci = getClient(sessionid).getCube(cubeName);
     if (!(ci instanceof Cube)) {
@@ -1355,6 +1356,7 @@ public class CubeMetastoreServiceImpl extends LensService implements CubeMetasto
         }
       }
     }
+    release(sessionid);
     return latestDate;
   }
 }
