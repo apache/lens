b46e9f6deff0def07360d118f59332fa3e68c993
Arshad Matin <arshadmatin20@gmail.com>
2015-04-21 21:35:08 +0530

    LENS-243 : Add smoke test as a part of lens-regression (Arshad Matin via amareshwari)

---
+++ lens-regression/src/main/java/org/apache/lens/regression/util/Util.java
@@ -0,0 +1,118 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.lens.regression.util;
+
+import java.io.*;
+import java.util.Properties;
+
+
+import org.apache.log4j.Logger;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class Util {
+
+  private Util() {
+
+  }
+
+  private static final Logger LOGGER = Logger.getLogger(Util.class);
+  private static final String PROPERTY_FILE = "lens.properties";
+  private static Properties properties;
+
+  public static Properties getPropertiesObj(String filename) {
+    try {
+      if (properties == null) {
+        properties = new Properties();
+        LOGGER.info("filename: " + filename);
+        InputStream confStream = Util.class.getResourceAsStream("/" + filename);
+        properties.load(confStream);
+        confStream.close();
+      }
+      return properties;
+
+    } catch (IOException e) {
+      LOGGER.info(e.getStackTrace());
+    }
+    return null;
+  }
+
+  public static String getProperty(String property) {
+    Properties prop = Util.getPropertiesObj(PROPERTY_FILE);
+    return prop.getProperty(property);
+  }
+
+  public static String runRemoteCommand(String command) throws JSchException, IOException {
+    StringBuilder outputBuffer = new StringBuilder();
+    StringBuilder print = new StringBuilder();
+
+    String userName = Util.getProperty("lens.remote.username");
+    String host = Util.getProperty("lens.remote.host");
+    String password = Util.getProperty("lens.remote.password");
+
+    LOGGER.info("Running command : " + command + " on host : " + host + " with user as " + userName);
+
+    JSch jsch = new JSch();
+    Session session = jsch.getSession(userName, host, 22);
+    session.setPassword(password);
+
+    Properties config = new Properties();
+    config.put("StrictHostKeyChecking", "no");
+
+    session.setServerAliveInterval(10000);
+    session.setConfig(config);
+    session.connect(0);
+
+    ChannelExec channel = (ChannelExec) session.openChannel("exec");
+    InputStream commandOutput = channel.getInputStream();
+
+    channel.setCommand(command);
+    channel.connect();
+    int readByte = commandOutput.read();
+    char toAppend = ' ';
+    while (readByte != 0xffffffff) {
+      toAppend = (char) readByte;
+      outputBuffer.append(toAppend);
+      readByte = commandOutput.read();
+      if (toAppend == '\n') {
+        LOGGER.info(print.toString());
+        print = new StringBuilder();
+      } else {
+        print.append(toAppend);
+      }
+    }
+    channel.disconnect();
+    session.disconnect();
+    return outputBuffer.toString();
+  }
+
+  public static void writeFile(String fileName, String str) {
+    try {
+      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+      out.println(str);
+      out.close();
+    } catch (IOException e) {
+      LOGGER.info("File Exception : " + e);
+    }
+  }
+
+}
---
+++ lens-regression/src/test/java/org/apache/lens/regression/sanity/ITSmokeTest.java
@@ -0,0 +1,124 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.lens.regression.sanity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.apache.lens.regression.core.testHelper.BaseTestClass;
+import org.apache.lens.regression.util.Util;
+
+import org.apache.log4j.Logger;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.jcraft.jsch.JSchException;
+
+public class ITSmokeTest extends BaseTestClass {
+
+  private static final Logger LOGGER = Logger.getLogger(ITSmokeTest.class);
+  private final String resourceDir = "src/test/resources";
+  private final String resultFile = resourceDir + "/result.data";
+  private final String smokeOutput = resourceDir + "/output.txt";
+  private final String clientDir = Util.getProperty("lens.client.dir");
+  private final String dbName = "smoketest";
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp(Method method) throws Exception {
+    LOGGER.info("Test Name: " + method.getName());
+  }
+
+  @Test(enabled = true, groups = {"integration"})
+  public void smokeTest() throws Exception {
+
+    String exampleScript = clientDir + "/bin/run-examples.sh";
+    String sampleMetastoreCommand = "bash " + exampleScript + " sample-metastore -db " + dbName;
+    String populateMetastoreCommand = "bash " + exampleScript + " populate-metastore -db " + dbName;
+    String runQueriesCommand = "bash " + exampleScript + " runqueries -db " + dbName;
+
+    LOGGER.info("Creating schema : ");
+    String output = Util.runRemoteCommand(sampleMetastoreCommand);
+    System.out.println("Output : " + output);
+    LOGGER.info("Output : " + output);
+
+    LOGGER.info("Populating Metastore : ");
+    output = Util.runRemoteCommand(populateMetastoreCommand);
+    LOGGER.info("Output : " + output);
+
+    LOGGER.info("Running Queries in Background : ");
+    output = Util.runRemoteCommand("nohup " + runQueriesCommand + " > smoke.log 2>&1 &");
+    LOGGER.info("Output : " + output);
+
+    waitToComplete();
+
+    output = Util.runRemoteCommand("cat smoke.log");
+    LOGGER.info("Output : " + output);
+
+    Util.writeFile(smokeOutput, output);
+
+    Assert.assertTrue(output.contains("Successful queries 146"), "Some Queries Failed");
+    Assert.assertTrue(compareFile(smokeOutput, resultFile), "Result Validation Failed");
+  }
+
+  private void waitToComplete() throws IOException, JSchException, InterruptedException {
+    boolean loop = true;
+    while (loop) {
+      String output = Util.runRemoteCommand("ps -ef | grep org.apache.lens.examples.SampleQueries | grep -v \"grep\" ");
+      if (output.length() == 0) {
+        loop = false;
+      }
+      Thread.sleep(60000);
+    }
+
+  }
+
+  private boolean compareFile(String file1, String file2) throws IOException {
+    String skipDriverTime = "Driver run time in millis";
+    String skipTotalTime = "Total time for running examples(in millis)";
+    String skipTime = "Total time in millis";
+    BufferedReader reader1 = new BufferedReader(new FileReader(new File(file1)));
+    BufferedReader reader2 = new BufferedReader(new FileReader(new File(file2)));
+    boolean validate = true;
+    try {
+      String line1 = reader1.readLine();
+      String line2 = reader2.readLine();
+      while ((validate) && (line1 != null) && (line2 != null)) {
+        if (line1.contains(skipTime) || line1.contains(skipDriverTime) || line1.contains(skipTotalTime)) {
+          line1 = reader1.readLine();
+          line2 = reader2.readLine();
+        } else if (!line1.equalsIgnoreCase(line2)) {
+          validate = false;
+        } else {
+          line1 = reader1.readLine();
+          line2 = reader2.readLine();
+        }
+      }
+    } finally {
+      reader1.close();
+      reader2.close();
+    }
+    return validate;
+  }
+
+}
