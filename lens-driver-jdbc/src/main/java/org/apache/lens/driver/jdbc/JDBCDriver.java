5c25e68fe71670df6c72d3b8dc08fcbcad6011c4
SushilMohanty <sushilmohanty@apache.org>
2016-05-25 16:53:22 +0530

    LENS-1155 : Seeing INTERNAL_SERVER_ERROR for JDBC queries due to connection unavailability

--- lens-driver-jdbc/src/main/java/org/apache/lens/driver/jdbc/JDBCDriver.java
+++ lens-driver-jdbc/src/main/java/org/apache/lens/driver/jdbc/JDBCDriver.java
@@ -94,6 +94,7 @@ public class JDBCDriver extends AbstractLensDriver {
   private ExecutorService asyncQueryPool;
 
   /** The query context map. */
+  @Getter
   private ConcurrentHashMap<QueryHandle, JdbcQueryContext> queryContextMap;
 
   /** The conf. */
@@ -458,7 +459,7 @@ public class JDBCDriver extends AbstractLensDriver {
     final int maxPoolSize = parseInt(this.conf.get(JDBC_POOL_MAX_SIZE.getConfigKey()));
     final int maxConcurrentQueries
       = parseInt(this.conf.get(MaxConcurrentDriverQueriesConstraintFactory.MAX_CONCURRENT_QUERIES_KEY));
-    checkState(maxPoolSize == maxConcurrentQueries, "maxPoolSize:" + maxPoolSize + " maxConcurrentQueries:"
+    checkState(maxPoolSize >= maxConcurrentQueries, "maxPoolSize:" + maxPoolSize + " maxConcurrentQueries:"
       + maxConcurrentQueries);
 
     queryContextMap = new ConcurrentHashMap<>();
