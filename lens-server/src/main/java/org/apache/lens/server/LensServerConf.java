d3b41b38dd5a7b8cb0493c84d3b30e6cbbd76e90
Rajashekhar Choukimath <rajashekhar.c@inmobi.com>
2018-08-16 16:00:31 +0530

    Fix for RNA-10440: Lens driver not picking hadoop conf

--- lens-server/src/main/java/org/apache/lens/server/LensServerConf.java
+++ lens-server/src/main/java/org/apache/lens/server/LensServerConf.java
@@ -42,7 +42,7 @@ public final class LensServerConf {
     static {
       HIVE_CONF.addResource("lensserver-default.xml");
       HIVE_CONF.addResource("lens-site.xml");
-      Configuration conf = new Configuration(false);
+      Configuration conf = new Configuration(true);
       conf.addResource("lens-site.xml");
       for (Map.Entry<String, String> prop : conf) {
         if (!prop.getKey().startsWith(LensConfConstants.SERVER_PFX)) {
