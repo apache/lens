410bfc033039812b42eba7d048012500c1746702
Rajashekhar <rajashekhar.c@inmobi.com>
2019-08-08 12:25:59 +0530

    fixed the code review comments

--- lens-server/src/main/java/org/apache/lens/server/query/LensServerDAO.java
+++ lens-server/src/main/java/org/apache/lens/server/query/LensServerDAO.java
@@ -869,9 +869,9 @@ public class LensServerDAO {
     }
   }
 
-  public PreparedLensQuery getPreparedQuery(String handle) {
+  public PreparedLensQuery getPreparedQuery(String handle) throws LensException {
     ResultSetHandler<PreparedLensQuery> rsh = new BeanHandler<>(PreparedLensQuery.class,
-        new BasicRowProcessor(new FinishedLensQueryBeanProcessor()));
+        new BasicRowProcessor(new BeanProcessor()));
     String sql = "select * from prepared_queries where handle=?";
     QueryRunner runner = new QueryRunner(ds);
     try {
@@ -879,7 +879,7 @@ public class LensServerDAO {
       return preparedLensQuery;
     } catch (SQLException e) {
       log.error("SQL exception while executing query.", e);
+      throw new LensException(e);
     }
-    return null;
   }
 }
